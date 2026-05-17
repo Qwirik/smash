be59f7f805c338c5081a4e0c266e91274b68e3b7
Tseren-Ubushieva Izhilina <itsubmiloopse@gmail.com>
2026-05-17 22:15:44 +0300

    Update s.py

--- s.py
+++ s.py
@@ -1,9 +1,27 @@
+import os
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from google import genai
 
-# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+
+app = FastAPI()
+
 client = genai.Client()
 
-response = client.models.generate_content(
-    model="gemini-3-flash-preview", contents="Tell me about how to take grant to study abroad"
-)
-print(response.text)
+class QueryRequest(BaseModel):
+    prompt: str
+
+@app.post("/generate")
+async def generate_intellect(request: QueryRequest):
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash", 
+            contents=request.prompt
+        )
+        return {"status": "success", "text": response.text}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
